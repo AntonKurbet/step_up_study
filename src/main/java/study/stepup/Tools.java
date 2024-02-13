e91d750d762bab2e92b1962b83f452d71b51aa98
Anton Kurbet <anton.kurbet@gmail.com>
2024-02-13 21:32:55 +0300

    refactor tool

--- src/main/java/study/stepup/Tools.java
+++ src/main/java/study/stepup/Tools.java
@@ -11,31 +11,33 @@ import java.util.Comparator;
 import java.util.stream.Stream;
 
 public class Tools {
+
+    public static final String ROOT_PATH = System.getProperty("user.dir");
+    public static final String SRC_DIR_PATH = "/src/main/java/study/stepup/lab3";
+    public static final String TARGET_FILE_PATH = "/src/main/resources/lab3.txt";
+    public static final String LINE_DELIMITER = "\n---------\n";
+    public static final Comparator<Path> FILENAME_STRING_COMPARATOR = Comparator.comparing(o -> o.getFileName().toString());
+    public static final char NEW_LINE = '\n';
+
     public static void main(String[] args) {
         try (
-                Stream<Path> stream = Files.list(Paths.get(System.getProperty("user.dir") + "/src/main/java/study/stepup/lab3"));
-                FileWriter fileWriter = new FileWriter(System.getProperty("user.dir") + "/src/main/resources/lab3.txt")
+                Stream<Path> stream = Files.list(Paths.get(ROOT_PATH + SRC_DIR_PATH));
+                FileWriter fileWriter = new FileWriter(ROOT_PATH + TARGET_FILE_PATH)
         ) {
             var files = stream
                     .filter(file -> !Files.isDirectory(file))
-                    .sorted(new Comparator<>() {
-                        @Override
-                        public int compare(Path o1, Path o2) {
-                            return o1.getFileName().toString().compareTo(o2.getFileName().toString());
-                        }
-                    })
+                    .sorted(FILENAME_STRING_COMPARATOR)
                     .toList();
-            fileWriter.write('\n');
+            fileWriter.write(NEW_LINE);
             for (var file : files) {
                 fileWriter.append(file.getFileName().toString());
-                fileWriter.append("\n---------\n");
-                try (var inputStream = new FileInputStream(file.toFile());) {
-                    CharSequence buffer = new String(inputStream.readAllBytes(), StandardCharsets.UTF_8);
-                    fileWriter.append(buffer);
+                fileWriter.append(LINE_DELIMITER);
+                try (var inputStream = new FileInputStream(file.toFile())) {
+                    fileWriter.append(new String(inputStream.readAllBytes(), StandardCharsets.UTF_8));
                 } catch (IOException e) {
                     throw new RuntimeException(e);
                 }
-                fileWriter.append('\n');
+                fileWriter.append(NEW_LINE);
             }
         } catch (IOException e) {
             throw new RuntimeException(e);
